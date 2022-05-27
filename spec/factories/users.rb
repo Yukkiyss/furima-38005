b166ce7b62228f9d27486d044172a641e65a058a
Yukkiyss <104711063+Yukkiyss@users.noreply.github.com>
2022-05-27 16:31:20 +0900

    FactoryBot記述編集

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
-    password_confirmation {password}
+    nickname              {"toshio"}
+    email                 {"sample@gmail.com"}
+    password              {"a123456"}
+    encrypted_password    {"a123456"}
+    last_name             {"鈴木"}
+    first_name            {"敏夫"}
+    last_name_kana        {"スズキ"}
+    first_name_kana       {"トシオ"}
+    birth_day             {"2000-01-01"}
   end
 end
