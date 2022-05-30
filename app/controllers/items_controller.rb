bcb151bda603031e028a24d081ffcf5f2890d5f5
Yukkiyss <104711063+Yukkiyss@users.noreply.github.com>
2022-05-30 21:49:53 +0900

    createアクションルーティング記述　itemsコントローラーnewアクション記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,11 +5,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:item_name, :item_price, :item_detail, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :item_price, :item_explanation_id, :category_id, :delivery_chage_id, :shipment_source_prefecture_id, :send_date_id, :item_detail, :image).merge(user_id: current_user.id)
   end
 
 end
