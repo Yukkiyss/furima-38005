4fb2f821b37b5c2973330a8791739217cac4c421
Yukkiyss <104711063+Yukkiyss@users.noreply.github.com>
2022-05-30 14:20:23 +0900

    Active Storageインストール

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+  def prototype_params
+    params.require(:item).permit(:item_name, :item_price, :item_detail, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :order
+  has_one_attached :image
 
   validates :item_name, presence: true
   validates :item_price, presence: true
