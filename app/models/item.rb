facfe700c40c9ec00b491d16f724d9d8b4a4840a
Yukkiyss <104711063+Yukkiyss@users.noreply.github.com>
2022-05-30 16:11:55 +0900

    ActiveHashインストール・ファイル作成・記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,4 +12,19 @@ class Item < ApplicationRecord
   validates :send_date_id, presence: true
   validates :item_detail, presence: true
 
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :delivery_chage_id
+    validates :item_explanation_id
+    validates :shipment_source_prefecture_id
+    validates :send_date_id
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :delivery_chage
+  belongs_to :item_explanation
+  belongs_to :shipment_source_prefecture
+  belongs_to :send_date
+
 end
