4c90ac3d9397c0c0f06a2400369ca79523e99842
Yukkiyss <104711063+Yukkiyss@users.noreply.github.com>
2022-05-30 14:10:38 +0900

    itemモデル作成・バリデーション記述

---
+++ app/models/item.rb
@@ -0,0 +1,14 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_one :order
+
+  validates :item_name, presence: true
+  validates :item_price, presence: true
+  validates :item_explanation_id, presence: true
+  validates :category_id, presence: true
+  validates :delivery_chage_id, presence: true
+  validates :shipment_source_prefecture_id, presence: true
+  validates :send_date_id, presence: true
+  validates :item_detail, presence: true
+
+end
