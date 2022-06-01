7261e63b69f6a9ea2cddc05170157f586ae42d10
Yukkiyss <104711063+Yukkiyss@users.noreply.github.com>
2022-06-01 14:49:55 +0900

    itemモデルバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   validates :item_name, presence: true
   validates :item_detail, presence: true
   validates :image, presence: true
-  validates :item_price, presence: true, format: { with: /\A[0-9]+\z/ }, numericality: { only_integer: true,greater_than: 300, less_than: 9_999_999 }
+  validates :item_price, presence: true, numericality: { only_integer: true,greater_than: 300, less_than: 9_999_999 }
 
   with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
