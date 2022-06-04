bd53cb145ace4689d69754366188adae18fe9319
Yukkiyss <104711063+Yukkiyss@users.noreply.github.com>
2022-06-04 16:44:32 +0900

    Rubocop 実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,7 +36,7 @@ class ItemsController < ApplicationController
 
   def destroy
     if current_user.id == @item.user_id
-      @item.destroy 
+      @item.destroy
       redirect_to root_path
     end
   end
@@ -56,5 +56,4 @@ class ItemsController < ApplicationController
   def item_params_id
     @item = Item.find(params[:id])
   end
-
 end
