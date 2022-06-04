0d751c6b22c2d4a6e3b1d8f04d45a08b8f189363
Yukkiyss <104711063+Yukkiyss@users.noreply.github.com>
2022-06-04 15:29:25 +0900

    updateアクション修正・privateメソッド追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_root_path, only: [:edit]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -26,10 +27,27 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end 
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:item_name, :item_price, :item_explanation_id, :category_id, :delivery_chage_id,
                                  :shipment_source_prefecture_id, :send_date_id, :item_detail, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_root_path
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
 end
