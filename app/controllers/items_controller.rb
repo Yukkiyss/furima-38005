697f78f3f2612ad104f811d24ef6fedabce85e24
Yukkiyss <104711063+Yukkiyss@users.noreply.github.com>
2022-06-04 16:03:57 +0900

    itemsコントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_root_path, only: [:edit]
+  before_action :item_params_id, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -47,4 +45,9 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
+
+  def item_params_id
+    @item = Item.find(params[:id])
+  end
+
 end
