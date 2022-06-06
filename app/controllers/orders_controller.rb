311d30d2ea93d98f11ae3feb182318d148218bdd
Yukkiyss <104711063+Yukkiyss@users.noreply.github.com>
2022-06-06 22:17:56 +0900

    ordersコントローラーindexアクション修正・indexビューファイル修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,13 @@ class OrdersController < ApplicationController
 
   def create
     binding.pry
+    @order_domicile = OrderDomicile.new(order_params)
+    @order_domicile.save
+  end
+
+  def order_params
+    params.require(:order_domicile).permit(:postcode, :shipment_source_prefecture_id, :buyer_city, :buyer_block, 
+                   :buyer_building,:buyer_phone_number, :order_id).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
