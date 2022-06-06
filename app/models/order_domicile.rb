787f1f250c3d2292f1e06e181b01a60fcc651993
Yukkiyss <104711063+Yukkiyss@users.noreply.github.com>
2022-06-06 16:02:49 +0900

    Formオブジェクトを作成

---
+++ app/models/order_domicile.rb
@@ -0,0 +1,25 @@
+class OrderDomicile
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postcode, :shipment_source_prefecture_id, :buyer_city, :buyer_block, :buyer_building, :buyer_phone_number
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :buyer_city
+    validates :buyer_block
+    validates :buyer_phone_number, numericality: { only_integer: true }
+    end
+
+    validates :shipment_source_prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :buyer_phone_number, length: { minimum: 10, maximum: 11 }
+
+    def save
+      order = Order.create(user_id: user_id, item_id: item_id)
+      Domicile.create(postcode: postcode, shipment_source_prefecture_id: shipment_source_prefecture_id, buyer_city: buyer_city, 
+      buyer_block: buyer_block, buyer_building: buyer_building,
+      buyer_phone_number: buyer_phone_number, order_id: order.id, user_id: user_id, item_id: item_id)
+    end
+      
+
+end
