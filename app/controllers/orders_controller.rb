54b29e75ee1ec3524fcaa5daf40cf7a365178a23
Yukkiyss <104711063+Yukkiyss@users.noreply.github.com>
2022-06-07 15:41:32 +0900

    createアクション　ストロングパラメーター修正　indexビューファイル記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,13 +7,18 @@ class OrdersController < ApplicationController
 
   def create
     # binding.pry
+    @item = Item.find(params[:item_id])
     @order_domicile = OrderDomicile.new(order_params)
+    if @order_domicile.valid?
     @order_domicile.save
+    return redirect_to root_path
+    end
+  render 'index'
   end
 
   def order_params
     params.require(:order_domicile).permit(:postcode, :shipment_source_prefecture_id, :buyer_city, :buyer_block, 
-                   :buyer_building,:buyer_phone_number, :order_id).merge(user_id: current_user.id, item_id: params[:item_id])
+                   :buyer_building,:buyer_phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
